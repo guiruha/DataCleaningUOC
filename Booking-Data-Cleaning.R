495959836fac089d5ada364eff21bd5442b38135
Gerard Alcalde <gerardj.ag@gmail.com>
2023-01-08 13:18:30 +0100

    Add Data Analysis

--- Booking-Data-Cleaning.R
+++ Booking-Data-Cleaning.R
@@ -232,3 +232,273 @@ kable(booking[is.na(booking$min_price),] %>%
         tail(5))
 
 
+
+########### ANALISIS DE LOS DATOS ###############
+## Estudio de la correlacion
+# Transformamos las variables a numérico
+booking$postal_code <- as.numeric(booking$postal_code)
+booking$latitude <- as.numeric(booking$latitude)
+booking$longitude <- as.numeric(booking$longitude)
+booking$free_wifi <- as.numeric(booking$free_wifi)
+booking$is_apartment <- as.numeric(booking$is_apartment)
+booking$pet_friendly <- as.numeric(booking$pet_friendly)
+booking$page_count <- booking$page_count + 1
+
+# Seleccionamos solo las variables numéricas
+booking_numeric <- booking %>%
+  dplyr::select(-c("city", "month", "as.integer(!is.na(NA))"))
+
+mattmp <- booking_numeric %>%
+  dplyr::select(c("adults", "children", "num_rooms", "postal_code", "latitude", "longitude", "hotel_score", "length_description", 
+                  "is_suite", "is_apartment", "has_free_cancelation", "min_price","max_price", "current_page", "in_page_count", "page_count"))
+
+sapply(mattmp, function(y) sum(is.na(y)))
+
+# Creamos una matriz de correlación del dataset
+cor_mat <- cor(mattmp, method = "spearman")
+
+mattmp <- booking_numeric %>%
+  dplyr::select(c("hotel_score", "staff_score", "facilities_score", "cleanliness_score", "comfort_score", "value_for_money_score", "location_score",
+                  "mean_flights", "free_wifi_score", "is_december", "is_march", "min_price", "max_price", "current_page", "in_page_count", "page_count"))
+
+sapply(mattmp, function(y) sum(is.na(y)))
+
+# Creamos una matriz de correlación del dataset
+cor_mat <- cor(mattmp, method = "spearman")
+mattmp <- booking_numeric %>%
+  dplyr::select(c("balcony", "swimming_pool", "pet_friendly", "kitchen", "city_view", "is_apartment", "non_smoking_rooms", "hotel_score", 
+                  "private_bathroom", "elevator", "heating", "safe", "air_conditioning", "free_wifi", "max_price", "min_price", "current_page", "in_page_count", "page_count"))
+
+sapply(mattmp, function(y) sum(is.na(y)))
+
+# Creamos una matriz de correlación del dataset
+cor_mat <- cor(mattmp, method = "spearman")
+
+# Filtramos para obtener cuales son las variables más correlacionadas con nuestra variable objetivo, current_page
+cor_mat <- cor(booking_numeric, method = "spearman")
+cor_mat_df <- as.data.frame(cor_mat)
+cor_mat_df %>%
+  filter(abs(page_count) > 0.065) %>%
+  filter(page_count < 0.95) %>%
+  arrange(page_count) %>%
+  dplyr::select(page_count)
+
+booking_pcount <- booking_numeric %>%
+  dplyr::select(c("kitchen", "is_apartment", "adults", "air_conditioning", "elevator", "private_bathroom",
+                  "num_rooms", "value_for_money_score", "heating", "non_smoking_rooms", "comfort_score", "hotel_score",
+                  "longitude", "has_free_cancelation", "page_count"))
+
+# Filtramos para obtener cuales son las variables más correlacionadas con nuestra variable objetivo, page_count
+cor_mat <- cor(booking_numeric, method = "spearman")
+cor_mat_df <- as.data.frame(cor_mat)
+cor_mat_df %>%
+  filter(abs(max_price) > 0.1) %>%
+  filter(max_price < 0.95) %>%
+  arrange(max_price) %>%
+  dplyr::select(max_price)
+
+booking_price <- booking_numeric %>%
+  dplyr::select(c("postal_code", "balcony", "pet_friendly", "latitude", "air_conditioning", "private_bathroom", "free_wifi", "staff_score",
+                  "swimming_pool", "location_score", "non_smoking_rooms", "mean_flights", "length_description", "facilities_score", "hotel_score",
+                  "safe", "cleanliness_score", "comfort_score", "max_price"))
+
+
+
+## Normalidad y homogeneidad de la varianza
+
+# Seleccion de los datos
+norm_cols <- c("value_for_money_score", "comfort_score", "hotel_score", "staff_score", "cleanliness_score", "location_score", "facilities_score", "length_description", "min_price", "max_price", "page_count")
+
+booking_norm <- booking_numeric %>%
+  dplyr::select(norm_cols)
+
+par(mfrow=c(2,2))
+for(i in 1:ncol(booking_norm)){
+  qqnorm(booking_norm[,i], main = paste("QQ-Plot de",colnames(booking_norm)[i]), col = "navy")
+  qqline(booking_norm[,i], col = "red")
+  hist(booking_norm[,i], main = paste("Histograma de", colnames(booking_norm)[i]), xlab=colnames(booking_norm)[i], freq = FALSE)
+}
+
+# Aplicamos el test de Shapiro-Wilk
+lapply(booking_norm, shapiro.test)
+
+# Obtenemos los Q-Q plots
+par(mfrow=c(2,3))
+for(i in 1:ncol(booking_norm)){
+  b <- boxcox(lm(booking_norm[,i] ~ 1))
+  lambda <- b$x[which.max(b$y)]
+  qqnorm((booking_norm[,i]^lambda - 1)/lambda, main = paste("QQ-Plot de",colnames(booking_norm)[i]), col = "navy")
+  qqline((booking_norm[,i]^lambda - 1)/lambda, col = "red")
+  hist((booking_norm[,i]^lambda - 1)/lambda, main = paste("Histograma de", colnames(booking_norm)[i]), xlab=colnames(booking_norm)[i], freq = FALSE)
+}
+
+# Flinger test para evaluar la homogeneidad de la varianza
+fligner.test(page_count ~ air_conditioning, data = booking)
+fligner.test(page_count ~ free_wifi, data = booking)
+
+booking <- booking %>%
+  mutate(high_hotel_score = ifelse(hotel_score >= 9, 1, 0))
+fligner.test(page_count ~ high_hotel_score, data = booking)
+
+booking <- booking %>%
+  mutate(high_max_price = ifelse(max_price >= mean(max_price), 1, 0))
+fligner.test(page_count ~ high_max_price, data = booking)
+
+
+## Contraste de hipotesis
+# Hipotesis 1
+
+# Buscamos el conjunto de datos de estudio:
+X1 <- booking %>%
+  filter(air_conditioning==1) %>%
+  dplyr::select("page_count")
+X1 <- as.vector(unlist(X1),'numeric')
+
+X2 <- booking %>%
+  filter(air_conditioning==0) %>%
+  dplyr::select("page_count")
+X2 <- as.vector(unlist(X2),'numeric')
+
+# Buscamos la varianza de ambas variables
+var1 <- var(X1)
+var2 <- var(X2)
+
+# Definimos el nivel de confianza al 95%
+alfa <- 0.05
+
+# Buscamos las medias y la el número de registros en cada variable
+mean1 <- mean(X1); n1 <- length(X1)
+mean2 <- mean(X2); n2 <- length(X2)
+
+# Calculamos el valor observado y el valor crítico
+zobs <- (mean1-mean2)/sqrt(var1/n1 + var2/n2)
+zcrit <- qnorm(1-alfa)
+
+# Finalmente calculamos el pvalue
+pvalue <- pnorm(zobs, lower.tail=TRUE)
+print(paste("The p-value obtained is: ", pvalue))
+# Mostramos las medias de las dos variables
+print(paste("The mean of the group 1 is: ", mean(X1)))
+print(paste("The mean of the group 2 is: ", mean(X2)))
+
+
+# Hipotesis 2
+
+# Buscamos el conjunto de datos de estudio:
+X1 <- booking %>%
+  filter(has_free_cancelation==1) %>%
+  dplyr::select("page_count")
+X1 <- as.vector(unlist(X1),'numeric')
+
+X2 <- booking %>%
+  filter(has_free_cancelation==0) %>%
+  dplyr::select("page_count")
+X2 <- as.vector(unlist(X2),'numeric')
+
+# Buscamos la varianza de ambas variables
+var1 <- var(X1)
+var2 <- var(X2)
+
+# Definimos el nivel de confianza al 95%
+alfa <- 0.05
+
+# Buscamos las medias y la el número de registros en cada variable
+mean1 <- mean(X1); n1 <- length(X1)
+mean2 <- mean(X2); n2 <- length(X2)
+
+# Calculamos el valor observado y el valor crítico
+zobs <- (mean1-mean2)/sqrt(var1/n1 + var2/n2)
+zcrit <- qnorm(1-(alfa/2))
+
+# Finalmente calculamos el pvalue
+pvalue <- pnorm(zobs, lower.tail=TRUE)
+print(paste("The p-value obtained is: ", pvalue))
+# Mostramos las medias de las dos variables
+print(paste("The mean of the group 1 is: ", mean(X1)))
+print(paste("The mean of the group 2 is: ", mean(X2)))
+
+
+# Hipotesis 3
+
+# Buscamos el conjunto de datos de estudio:
+X1 <- booking %>%
+  filter(hotel_score>=9.0) %>%
+  dplyr::select("page_count")
+X1 <- as.vector(unlist(X1),'numeric')
+
+X2 <- booking %>%
+  filter(hotel_score<9.0) %>%
+  dplyr::select("page_count")
+X2 <- as.vector(unlist(X2),'numeric')
+
+# Buscamos la varianza de ambas variables
+var1 <- var(X1)
+var2 <- var(X2)
+
+# Definimos el nivel de confianza al 95%
+alfa <- 0.05
+
+# Buscamos las medias y la el número de registros en cada variable
+mean1 <- mean(X1); n1 <- length(X1)
+mean2 <- mean(X2); n2 <- length(X2)
+
+# Calculamos el valor observado y el valor crítico
+zobs <- (mean1-mean2)/sqrt(var1/n1 + var2/n2)
+zcrit <- qnorm(1-alfa)
+
+# Finalmente calculamos el pvalue
+pvalue <- pnorm(zobs, lower.tail=TRUE)
+print(paste("The p-value obtained is: ", pvalue))
+# Mostramos las medias de las dos variables
+print(paste("The mean of the group 1 is: ", mean(X1)))
+print(paste("The mean of the group 2 is: ", mean(X2)))
+
+
+# Hipotesis 4
+
+# Buscamos el conjunto de datos de estudio:
+X1 <- booking %>%
+  filter(min_price<mean(min_price)) %>%
+  dplyr::select("page_count")
+X1 <- as.vector(unlist(X1),'numeric')
+
+X2 <- booking %>%
+  filter(min_price>=mean(min_price)) %>%
+  dplyr::select("page_count")
+X2 <- as.vector(unlist(X2),'numeric')
+
+# Buscamos la varianza de ambas variables
+var1 <- var(X1)
+var2 <- var(X2)
+
+# Definimos el nivel de confianza al 95%
+alfa <- 0.05
+
+# Buscamos las medias y la el número de registros en cada variable
+mean1 <- mean(X1); n1 <- length(X1)
+mean2 <- mean(X2); n2 <- length(X2)
+
+# Calculamos el valor observado y el valor crítico
+zobs <- (mean1-mean2)/sqrt(var1/n1 + var2/n2)
+zcrit <- qnorm(1-alfa)
+
+# Finalmente calculamos el pvalue
+pvalue <- pnorm(zobs, lower.tail=TRUE)
+print(paste("The p-value obtained is: ", pvalue))
+# Mostramos las medias de las dos variables
+print(paste("The mean of the group 1 is: ", mean(X1)))
+print(paste("The mean of the group 2 is: ", mean(X2)))
+
+
+## Regresion lineal
+# Regresion lineal definitiva para page_count
+#Create the linear model
+mrl_final <- lm(page_count~staff_score + value_for_money_score + free_wifi_score 
+                + balcony + pet_friendly + air_conditioning + swimming_pool 
+                + mean_flights + length_description, data=booking)
+
+# Regresion lineal definitiva para max_price
+#Create the linear model
+mrl_price <- lm(max_price~staff_score + location_score + balcony + air_conditioning 
+                + free_wifi + swimming_pool + non_smoking_rooms + mean_flights 
+                + length_description + safe, data=booking)
